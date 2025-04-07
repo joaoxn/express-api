c364e1ace8eee1e0f15faaaebd27a97937f387e2
João Olivo <jvso@yahoo.com>
2025-04-07 18:48:47 -0300

    Import new conversion package + refactor

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 import * as http from 'http';
 import * as url from 'url';
-import {Centimeter, Kilometer, Meter, Unit} from "./converter.ts";
+import {get} from "oop-units-converter";
 
 
 const PORT = 8000;
@@ -12,71 +12,31 @@ const server = express();
 server.get('/', (req: express.Request, res: express.Response) => {
     res.status(200).send('OK');
 });
-
-server.get('/km/:goal/:value', (req: express.Request, res: express.Response) => {
-    const goal = req.params.goal;
+server.get('/:value/:source/:goal', (req: express.Request, res: express.Response) => {
     const value = Number(req.params.value);
+    const source = req.params.source;
+    const goal = req.params.goal;
+
     if (isNaN(value)) {
-        res.status(400).send('Invalid value');
+        res.status(400).send("Could not parse amount for unit " +
+            "because it is not specified correctly in the first parameter. " +
+            "Consider the following order: /amount/unit/new-unit");
         return;
     }
-    const km = new Kilometer(value);
-    let result: Unit;
-    switch (goal) {
-        case 'm':  result = km.toMeter();       break;
-        case 'cm': result = km.toCentimeter();  break;
-        case 'mi': result = km.toMile();        break;
-        case 'ft': result = km.toFeet();        break;
-        case 'in': result = km.toInch();        break;
-        default:
-            res.status(400).send('Invalid goal');
-            return;
-    }
-    res.status(200).send(result.value);
-});
 
-server.get('/m/:goal/:value', (req: express.Request, res: express.Response) => {
-    const goal = req.params.goal;
-    const value = Number(req.params.value);
-    if (isNaN(value)) {
-        res.status(400).send('Invalid value');
+    const unit = get(value, source);
+    if (!unit) {
+        res.status(400).send("Cannot convert unit because it's acronym does not exist");
         return;
     }
-    const meter = new Meter(value);
-    let result: Unit;
-    switch (goal) {
-        case 'km': result = meter.toFeet().toMile().toKilometer();  break;
-        case 'cm': result = meter.toFeet().toInch().toCentimeter(); break;
-        case 'mi': result = meter.toFeet().toMile();                break;
-        case 'ft': result = meter.toFeet();                         break;
-        case 'in': result = meter.toFeet().toInch();                break;
-        default:
-            res.status(400).send('Invalid goal');
-            return;
-    }
-    res.status(200).send(result.value);
-});
 
-server.get('/cm/:goal/:value', (req: express.Request, res: express.Response) => {
-    const goal = req.params.goal;
-    const value = Number(req.params.value);
-    if (isNaN(value)) {
-        res.status(400).send('Invalid value');
+    const newUnit = unit.to(goal);
+    if (!newUnit) {
+        res.status(400).send("Cannot convert to goal unit because it's acronym does not exist");
         return;
     }
-    const cm = new Centimeter(value);
-    let result: Unit;
-    switch (goal) {
-        case 'km': result = cm.toMeter().toFeet().toMile().toKilometer();   break;
-        case 'm':  result = cm.toMeter();                                   break;
-        case 'mi': result = cm.toMeter().toFeet().toMile();                 break;
-        case 'ft': result = cm.toMeter().toFeet();                          break;
-        case 'in': result = cm.toInch();                                    break;
-        default:
-            res.status(400).send('Invalid goal');
-            return;
-    }
-    res.status(200).send(result.value);
+
+    res.status(200).send(newUnit.value);
 });
 
 server.listen(PORT, () => {
